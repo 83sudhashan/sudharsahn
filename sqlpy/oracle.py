2d0bd7c23afe56cbfaee9ce96138b6984f214c05
83sudhashan <155879464+83sudhashan@users.noreply.github.com>
2024-01-27 14:58:03 +0530

    Add files via upload

--- sqlpy/oracle.py
+++ sqlpy/oracle.py
@@ -12,13 +12,12 @@ def  createtable():
     cur.execute(query)
 def insertrecord(sid,name):
     record={'id':str(sid),'name':name}
-    
     #query="insert into sudharshan45mca(id,name) value())",record
     #query='''insert into sudharshan45mca(id,name) values(1,'neeraj kumar')'''
     #query='''insert into sudharshan45mca(id,name) values(2,'ummaleti')'''
     cur.execute("insert into sudharshan45mca(id,name) values(:id,:name)",record)
     res.commit()
-#insertrecord(0,'raghu')
+# insertrecord(0,'raghu')
 def read_records():
     query='select * from sudharshan45mca'
     cur.execute(query)
@@ -28,27 +27,37 @@ def read_records():
         data.writerow(['id','name']) 
         for row in records:
             data.writerow(row)
-read_records() 
+#read_records() 
+def insertdata():
+    with open('record.csv','r',newline='') as file:
+        record=csv.DictReader(file)
+        for i in  record:
+            cur.execute("insert into sudharshan45mca(id,name) values(:id,:name)",i)
+            res.commit()
+insertdata() 
   
-# def update_record(sid,name):
-#     record={'id':str(sid),'name':name}
-#     #"update sudharshan45mca set name='scientist' where id=1",record
-#     cur.execute("update sudharshan45mca set name=(:name) where id=(:id)",record)
-#     res.commit()
+def update_record(sid,name):
+    record={'id':str(sid),'name':name}
+    # "update sudharshan45mca set name='scientist' where id=1",record
+    cur.execute("update sudharshan45mca set name=(:name) where id=(:id)",record)
+    res.commit()
 # update_record(1,'edhar')   
-# def delete_record(sid):
-#     record={'id':str(sid)}
-#     #"update sudharshan45mca set name='scientist' where id=1",record
-#     cur.execute("delete from sudharshan45mca where id=(:id)",record)
-#     res.commit()
-# #delete_record(1,'suredhar')   
-# def fetch_record(sid):
-#     record={'id':str(sid)}
-#     cur.execute("select * from sudharshan45mca where id=(:id)",record)
-#     record=cur.fetchall()
-#     for i in record:
-#         print(i)
-# #fetch_record(3)   
+def delete_record(sid):
+    record={'id':str(sid)}
+     #"update sudharshan45mca set name='scientist' where id=1",record
+    cur.execute("delete from sudharshan45mca where id=(:id)",record)
+    res.commit()
+# delete_record(1,'suredhar')   
+def fetch_record(sid):
+    record={'id':str(sid)}
+    cur.execute("select * from sudharshan45mca where id=(:id)",record)
+    record=cur.fetchall()
+    for i in record:
+         print(i)
+# fetch_record(3)   
+def truncate():
+    query='truncate table sudharshan45mca'
+    cur.execute(query)
 
 
   
